4b096b3dff186c9a8365554e30bb2f6df43f9ccf
EloraBarua <elorabarua62@gmail.com>
2022-05-23 13:40:11 +0600

    CSS component library,header,carrocel,footer added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import image1 from '../../images/pavan-trikutam-avJ9uz9Qhcw-unsplash.jpg'
+import image2 from '../../images/pexels-rachel-claire-4846461.jpg'
+
+const Banner = () => {
+    return (
+        <Carousel infiniteLoop useKeyboardArrows autoPlay interval={5000} showThumbs={false} showStatus={false}>
+            <div>
+                <img src={image1} className='h-[600px]'/>
+            </div>
+            <div>
+                <img src={image2} className='h-[600px]' />
+            </div>
+            <div>
+                <img src={image1} className='h-[600px]'/>
+                
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
